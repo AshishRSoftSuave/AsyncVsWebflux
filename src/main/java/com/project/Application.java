dbbf86fa920cf30a5ab4ff4f22d6bab0ac737859
Ramesh Vayavuru <rameshv1210@gmail.com>
2021-03-09 19:05:54 +0530

    Webflux initial commit

--- src/main/java/com/project/Application.java
+++ src/main/java/com/project/Application.java
@@ -2,12 +2,9 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
